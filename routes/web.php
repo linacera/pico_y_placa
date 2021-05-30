fc09fdd28aab4246b048a78e8cbfbdadf8bade59
Johanna Cerezo <caro.cerezor@gmail.com>
2021-05-29 22:26:40 -0500

    Added post route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PredictorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('predictor');
 });
+
+Route::post('/predict',[PredictorController::class,'getInput']);
